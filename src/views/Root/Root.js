33ee5ad3eec50af87d172eb845068975197e58cf
Patryk Orłowski <orzeleagle122@gmail.com>
2020-12-22 13:33:28 +0100

    revised initial project structure

---
+++ src/views/Root/Root.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Root extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <>
+        <h1>
+          Hellow Word
+        </h1>
+      </>
+     );
+  }
+}
+ 
+export default Root;
