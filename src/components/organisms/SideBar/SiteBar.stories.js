408aeca18f42b428466e77bc40ab914daee092a0
Patryk Orłowski <orzeleagle122@gmail.com>
2020-12-29 17:50:39 +0100

    npm install --save-dev storybook-react-router

--- src/components/organisms/SideBar/SiteBar.stories.js
+++ src/components/organisms/SideBar/SiteBar.stories.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 import SiteBar from './SideBar';
+import StoryRouter from 'storybook-react-router';
 
 storiesOf('Organisms/SiteBar', module)
+    .addDecorator(StoryRouter())
     .add('SiteBar',()=><SiteBar />);
 
